e8f05af9b3f9fb01e000c4cef1f3b72a8dd8921e
Aleksey Potapov <aleksey.potapov1811@gmail.com>
2018-09-06 00:29:46 +0300

    finish homework-04

--- javascript/homework-04/js/index.js
+++ javascript/homework-04/js/index.js
@@ -10,22 +10,74 @@ const products = {
 
 function Cashier({ name, productDatabase }) {
   this.name = name;
-
   this.productDatabase = productDatabase;
-
-  this.getCustomerMoney = function(value = 0) {
+  this.customerMoney = 0;
+  this.totalPrice = 0;
+  this.surrenderResult = 0;
+  this.getCustomerMoney = function(value) {
     this.customerMoney = value;
   };
   this.countTotalPrice = function(order) {
-    orderKeys = Object.keys(order);
+    for (const key in order) {
+      this.totalPrice += productDatabase[key] * order[key];
+    }
+    return this.totalPrice;
+  };
+  this.countChange = function() {
+    if (this.customerMoney >= this.totalPrice) {
+      this.surrenderResult = this.customerMoney - this.totalPrice;
+    } else if (this.customerMoney < this.totalPrice) {
+      this.surrenderResult = null;
+    }
+    return this.surrenderResult;
+  };
+  this.onSuccess = function(change) {
+    console.log(`Спасибо за покупку, ваша сдача ${change}!`);
+  };
+  this.onError = function() {
+    console.log("Очень жаль, вам не хватает денег на покупки");
+  };
+  this.reset = function() {
+    this.customerMoney = 0;
   };
 }
 
-const vasya = new Cashier({ name: "Chuck", productDatabase: products });
+const mango = new Cashier({ name: "Mango", productDatabase: products });
 
+const order = {
+  bread: 2,
+  milk: 2,
+  apples: 1,
+  cheese: 1
+};
 
-vasya.countTotalPrice({
-  apples: 10,
-  chicken: 15,
-  cheese: 20
+console.log(mango.name);
+console.log(mango.productDatabase);
+console.log(mango.customerMoney);
+
+const totalPrice = mango.countTotalPrice({
+  bread: 2,
+  milk: 2,
+  apples: 1,
+  cheese: 1
 });
+
+console.log(totalPrice);
+
+mango.getCustomerMoney(300);
+
+console.log(mango.customerMoney);
+
+const change = mango.countChange();
+
+console.log(change);
+
+if (change !== null) {
+  mango.onSuccess(change);
+} else {
+  mango.onError();
+}
+
+mango.reset(); 
+
+console.log(mango.customerMoney);
