d4bf08d47c754e321238e2d025e1bebd354e10bf
Aleksey Potapov <aleksey.potapov1811@gmail.com>
2018-09-19 21:54:48 +0300

    completed AT hw6

--- javascript/Additional task!!!/module-06/index.js
+++ javascript/Additional task!!!/module-06/index.js
@@ -49,91 +49,108 @@
 // myString.showValue(); // '=^.^='
 
 //===============================TASK 3;
-/*
-  Создайте класс Car с указанными полями и методами.
-*/
+
 
 // class Car {
-//     constructor(maxSpeed) {
-//       /*
-//         Добавьте свойства:
-//           - speed - для отслеживания текущей скорости, изначально 0.
+//   constructor(maxSpeed) {
 
-//           - maxSpeed - для хранения максимальной скорости
+//     this.speed = 0;
+//     this.maxSpeed = maxSpeed;
+//     this.running = false;
+//     this.distance = 0;
 
-//           - running - для отслеживания заведен ли автомобиль,
-//             возможные значения true или false. Изначально false.
-
-//           - distance - содержит общий киллометраж, изначально с 0
-//       */
-//     }
-
-//     turnOn() {
-//       // Добавьте код для того чтобы завести автомобиль
-//       // Просто записывает в свойство running значание true
-//     }
-
-//     turnOff() {
-//       // Добавьте код для того чтобы заглушить автомобиль
-//       // Просто записывает в свойство running значание false
-//     }
-
-//     accelerate(spd) {
-//       // Записывает в поле speed полученное значение, при условии что
-//       // оно не больше чем значение свойства maxSpeed
-//     }
-
-//     decelerate(spd) {
-//       // Записывает в поле speed полученное значение, при условии что
-//       // оно не больше чем значение свойства maxSpeed и не меньше нуля
-//     }
-
-//     drive(hours) {
-//       // Добавляет в поле distance киллометраж (hours умноженное на значение поля speed),
-//       // но только в том случае если машина заведена!
+//   }
+//   turnOn() {
+//     return this.running = true;
+//   }
+//   turnOff() {
+//     return this.running = false;
+//   }
+//   accelerate(spd) {
+//     if (this.maxSpeed > spd) {
+//       return this.speed = spd;
 //     }
 //   }
+//   decelerate(spd) {
+//     if (this.maxSpeed > spd && spd > 0) {
+//       return this.speed = spd;
+//     }
+//   }
+//   drive(hours) {
+//     if (this.running) {
+//      return this.distance = hours * this.speed;
+//     };
+//   }
+// }
+
+// const lamborgini = new Car(460);
+// console.log(lamborgini);
+// lamborgini.turnOn();
+// lamborgini.accelerate(89)
+// lamborgini.decelerate(30);
+// lamborgini.drive(25)
 
 //==================================TASK 4;
-/*
-  Добавьте к классу Car из предыдущего задания статический
-  метод getSpecs, который получает объект-машину как аргумент
-  и выводит в консоль значения полей maxSpeed, running и distance.
-  
-  Использование будет выглядеть следующим образом:
-  
-  const someCar = new Car(100);
-  someCar.turnOn();
-  someCar.drive(2);
-  
-  Car.getSpecs(someCar); // maxSpeed: 100, running: true, distance: 200
-*/
-
-//==================================TASK 5;
-/*
-  Добавьте классу Car свойство value - цена автомобиля.
-  
-  Добавьте классу Car использование геттеров и сеттеров для свойства value.
-  
-  Геттер вернет текущей значение поля this._value
-  Сеттер запишет в поле this._value то что ему присвоят
-  
-  PS: имя геттера и сеттера не может совпадать с полем, поэтому используйте
-    не this.value а this._value
-    
-  Использование выглядит следующим образом:
-  
-  const myCar = new Car(50, 2000);
-  
-  myCar.value; // 2000
-  myCar.value = 4000;
-  myCar.value; // 4000
-*/
 
 // class Car {
-//     constructor(maxSpeed, value) {
-//       // ... код
-//       this._value = value;
-//     }
-//     // ... код
+//   constructor(maxSpeed) {
+
+//     this.speed = 0;
+//     this.maxSpeed = maxSpeed;
+//     this.running = false;
+//     this.distance = 0;
+
 //   }
+//   static getSpecs(car) {
+//     console.log(`maxSpeed : ${car.maxSpeed}, running: ${car.running}, distance: ${car.distance} `);
+//   }
+//   turnOn() {
+//     return this.running = true;
+//   }
+//   turnOff() {
+//     return this.running = false;
+//   }
+//   accelerate(spd) {
+//     if (this.maxSpeed > spd) {
+//       return this.speed = spd;
+//     }
+//   }
+//   decelerate(spd) {
+//     if (this.maxSpeed > spd && spd > 0) {
+//       return this.speed = spd;
+//     }
+//   }
+//   drive(hours) {
+//     if (this.running) {
+//       return this.distance = hours * this.speed;
+//     };
+//   }
+
+// }
+// const someCar = new Car(100);
+// someCar.turnOn();
+// someCar.drive(2);
+
+// Car.getSpecs(someCar);  
+// console.log(someCar);
+
+//==================================TASK 5;
+
+// class Car {
+//   constructor(maxSpeed, value) {
+//     this.maxSpeed = maxSpeed;
+//     this._value = value;
+//   }
+//   get value() {
+//     return this._value;
+//   }
+//   set value(value) {
+//     this._value = value;
+//   }
+
+// }
+// const myCar = new Car(50, 2000);
+
+// console.log(myCar.value); // 2000
+// console.log(myCar.value = 4000);
+// console.log(myCar.value); // 4000
