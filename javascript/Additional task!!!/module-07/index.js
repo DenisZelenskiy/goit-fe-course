39b97512df63b131c97eeda9c70ba105ecb10466
Aleksey Potapov <aleksey.potapov1811@gmail.com>
2018-10-03 20:38:52 +0300

    at 7

--- javascript/Additional task!!!/module-07/index.js
+++ javascript/Additional task!!!/module-07/index.js
@@ -1,85 +1,110 @@
 "use strict";
 
 //-------TASK 1;
-/*
-  Есть список категорий с классом categories (на вкладке HTML).
-  
-  Напишите код, который для каждого элемента li (первая вложенность) 
-  в списке categories выведет в консоль:
-  - Текст непосредственно в нём (название категории)
-  - Количество всех вложенных в него элементов li
-  
-  К примеру:
-    Категория: Животные
-    Количество вложенных li: 4
-*/
-const categories = document.querySelector(".categories");
-const item = [...categories.children];
-console.log(item.forEach(elem => console.log(elem.textContent)));
+
+// const list = document.querySelector('.categories');
+// const item = list.children;
+// const diplayText = (elem) => {
+//     for (let i = 0, max = elem.length; i < max; i += 1) {
+//         console.log('Категория :', elem[i].childNodes[0].textContent.trim());
+//         console.log("Количество вложенных li: ", elem[i].childNodes[1].children.length)
+//     }
+// }
+
+// diplayText(item);
 
 //----------TASK 2;
-/*
-  Дан список с классом .list
-	- Найдите первого потомка списка и сделайте его текст красного цвета
-	- Найдите последнего потомка списка и сделайте его текст синего цвета
-*/
+
+// const list = document.querySelector(".list");
+// list.firstElementChild.style.color = "red";
+// list.lastElementChild.style.color = 'blue';
+
 //--------TASK 3;
-/*
-  Дан ul склассом .list и массив строк. 
-  
-  Вставьте элементы этого массива в ul так, чтобы каждый элемент стоял в своем li.
-*/
 
 // const elements = ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS'];
-//-------TASK 4;
-/*
-  Напишите скрипт для создания списка ul.
-  
-  Для каждого пункта:
-    - Запрашивайте содержимое пункта li у пользователя с помощью prompt.
-    - Создавайте пункт и добавляйте его к ul.
-    - Процесс прерывается, когда пользователь нажимает Cancel.
-    - Все элементы списка должны создаваться динамически.
-*/
+// const list = document.querySelector('.list');
 
+// const itteration = () => i += 1;
+// let i = 0;
+
+// const addElement = (arr) => arr.map(elem => {
+//     elem = document.createElement('li')
+//     elem.textContent = arr[i];
+//     list.append(elem);
+//     itteration();
+// }
+// )
+// addElement(elements);
+
+// console.log(list);
+//-------TASK 4;
+
+// const body = document.body;
+// const list = document.createElement('ul');
+// body.prepend(list);
+// let input;
+// do {
+//     input = prompt('');
+//     if (input !== null) {
+//         list.append(document.createElement('li'));
+//         list.lastElementChild.textContent = input;
+//     }
+// }
+// while (input !== null);
+// console.log(body)
 //--------TASK 5;
-/*
-  Есть список с классом .size-filter из произвольного 
-  количества чекбоксов, каждый из которых содержит 
-  размер одежды в фильтре.
-  
-  Напишите функцию getInputsData(inputs), которая
-  принимает 1 параметр inputs - массив тех инпутов
-  у которых состояние checked.
-  
-  Возвращает массив значений атрибута value.
-*/
+
+// const sizeFilter = document.querySelector('.size-filter');
+// const inputValid = [...document.querySelectorAll('input')].filter(elem => elem.hasAttribute("checked"));
+// const getInputData = (inputs) => inputs.map(elem => elem.getAttribute("value"));
+// console.log(getInputData(inputValid));
 
 //--------TASK 6;
-/*
-  Создайте функцию createPostCard(), которая 
-  создает и возвращает DOM-узел карточки поста.
-  
-  Разметка с классами есть на вкладке HTML.
-  Стили на вкладке CSS.
-  
-  Используйте createElement для создания узлов.
-  Добавьте классы и атрибуты.
-*/
 
-//----------TASK 7;
-/*
-  В HTML-документе уже есть тег с id="root" (вкладка HTML)
-  
-  Создайте функцию createBoxes(num), которая принимает 1 параметр num - число.
-  
-  Функция создает столько div, сколько указано в num и возвращает их в одном
-  общем контейнере. После чего необходимо повесить результат работы функции
-  в div с id="#root"
-  
-  Каждый div:
-    - Имеет случайный rgb цвет фона
-    - Размеры самого первого div - 30px на 30px.
-    - Каждый следующий div после первого, должен быть шире и выше предыдущего
-      на 10px
-*/
+// const createPostCard = () => {
+//     const post = document.createElement('div');
+//     post.classList.add("post");
+//     const postImage = document.createElement('img');
+//     postImage.classList.add('post__image');
+//     postImage.setAttribute('src', 'http://via.placeholder.com/400x150');
+//     postImage.setAttribute('alt', 'post image');
+//     const postTitle = document.createElement('h2');
+//     postTitle.classList.add('post__title');
+//     postTitle.textContent = "Lorem ipsum dolor";
+//     const postText = document.createElement('p');
+//     postText.classList.add("post__text");
+//     postText.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos eatemporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamusconsequuntur, qui modi nesciunt veritatis distinctio rem!";
+//     const button = document.createElement('a');
+//     button.classList.add('button');
+//     button.setAttribute('href', '#');
+//     button.textContent = 'Read more';
+//     post.append(postImage,postTitle,postText,button);
+//     return post;
+// };
+// document.querySelector('.post').after(createPostCard());
+// document.querySelector('.post').after(createPostCard());
+// document.querySelector('.post').after(createPostCard());
+// document.querySelector('.post').after(createPostCard());
+// document.querySelector('.post').after(createPostCard());
+
+//---------TASK 7;
+
+// const root = document.querySelector("#root")
+// const genColor = () => "#" + ((1 << 24) * Math.random() | 0).toString(16);
+// const createBoxes = (num) => {
+//     const container = document.createElement('div');
+//     const list = [];
+//     for (let i = 0, width = 30, height = 30; i < num; i += 1, width += 10, height += 10) {
+//         let div = document.createElement('div');
+//         div.style.width = width + "px";
+//         div.style.height = height + "px";
+//         div.style.backgroundColor = genColor();
+//         list.push(div);
+//     }
+//     container.append(...list);
+//     return container;
+// };
+// root.append(createBoxes(99999));
+
+
+
