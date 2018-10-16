b11a54ab14e201d1ab142fbca080d8716524c589
Aleksey Potapov <aleksey.potapov1811@gmail.com>
2018-10-16 23:51:14 +0300

    hw8 class

--- javascript/homework-08/js/index.js
+++ javascript/homework-08/js/index.js
@@ -1,75 +1,172 @@
 "use strict";
-$on(document, "DOMContentLoaded", () => {
-  const imageGallery = $qs(".js-image-gallery");
-  const fullview = $cel("div", { className: "fullview" });
-  const imgFullview = $cel("img", { src: "#", alt: "" });
-  const galleryItems = [
-    {
-      preview: "img/preview-1.jpeg",
-      fullview: "img/fullview-1.jpeg",
-      alt: "alt text 1"
-    },
-    {
-      preview: "img/preview-2.jpeg",
-      fullview: "img/fullview-2.jpeg",
-      alt: "alt text 2"
-    },
-    {
-      preview: "img/preview-3.jpeg",
-      fullview: "img/fullview-3.jpeg",
-      alt: "alt text 3"
-    },
-    {
-      preview: "img/preview-4.jpeg",
-      fullview: "img/fullview-4.jpeg",
-      alt: "alt text 4"
-    },
-    {
-      preview: "img/preview-5.jpeg",
-      fullview: "img/fullview-5.jpeg",
-      alt: "alt text 5"
-    },
-    {
-      preview: "img/preview-6.jpeg",
-      fullview: "img/fullview-6.jpeg",
-      alt: "alt text 6"
-    }
-  ];
+// $on(document, "DOMContentLoaded", () => {
+//   const imageGallery = $qs(".js-image-gallery");
 
-  imageGallery.append(fullview);
-  fullview.append(imgFullview);
-  imageGallery.append(createListPreview(galleryItems));
+// const galleryItems = [
+//   {
+//     preview: "img/preview-1.jpeg",
+//     fullview: "img/fullview-1.jpeg",
+//     alt: "alt text 1"
+//   },
+//   {
+//     preview: "img/preview-2.jpeg",
+//     fullview: "img/fullview-2.jpeg",
+//     alt: "alt text 2"
+//   },
+//   {
+//     preview: "img/preview-3.jpeg",
+//     fullview: "img/fullview-3.jpeg",
+//     alt: "alt text 3"
+//   },
+//   {
+//     preview: "img/preview-4.jpeg",
+//     fullview: "img/fullview-4.jpeg",
+//     alt: "alt text 4"
+//   },
+//   {
+//     preview: "img/preview-5.jpeg",
+//     fullview: "img/fullview-5.jpeg",
+//     alt: "alt text 5"
+//   },
+//   {
+//     preview: "img/preview-6.jpeg",
+//     fullview: "img/fullview-6.jpeg",
+//     alt: "alt text 6"
+//   }
+// ];
 
-  function createListPreview(items) {
-    const list = $cel("ul", { className: "list" });
 
-    for (let item of items) {
-      let li = document.createElement("li");
-      let img = document.createElement("img");
-      img.setAttribute("src", item.preview);
-      img.setAttribute("data-fullview", item.fullview);
-      img.setAttribute("alt", item.alt);
-      li.appendChild(img);
-      list.appendChild(li);
-    }
+//   function createGallery(arr) {
 
-    const defaultFullview =
-      list.firstElementChild.firstElementChild.dataset.fullview;
+//     const fullview = $cel("div", { className: "fullview" });
+//     const imgFullview = $cel("img", { src: "#", alt: "" });
+//     const list = $cel("ul", { className: "list" });
+//     const result = [];
 
-    imgFullview.setAttribute("src", defaultFullview);
-    return list;
+//     fullview.append(imgFullview);
+
+//     createListPreview(galleryItems)
+
+//     function createListPreview(arr) {
+//       let i = 1;
+//       for (let item of arr) {
+//         let li = document.createElement("li");
+//         let img = document.createElement("img");
+//         img.setAttribute("src", item.preview);
+//         img.setAttribute("data-fullview", item.fullview);
+//         img.setAttribute("alt", item.alt);
+//         img.setAttribute('class', `index${i}`);
+//         li.appendChild(img);
+//         list.appendChild(li);
+//         i += 1;
+
+//       }
+//     }
+
+//     const defaultFullview = $qs('.index1', list).getAttribute("data-fullview");
+//     imgFullview.setAttribute("src", defaultFullview);
+
+//     result.push(fullview, list)
+
+
+//     $on(list, "click", changePreviewImg);
+
+//     function changePreviewImg(e) {
+//       const target = e.target;
+//       const srcFullview = target.dataset.fullview;
+//       const imgList = $qsa("img", list);
+
+
+//       if (target.nodeName !== "IMG") return;
+
+//       imgFullview.setAttribute("src", srcFullview);
+
+//       for (let img of imgList) {
+//         img.classList.remove('active');
+//       }
+
+
+//       target.className += " active";
+
+//     }
+
+//     return result
+
+//   }
+
+//   imageGallery.append(...createGallery(galleryItems))
+// });
+
+const galleryItems = [
+  {
+    preview: "img/preview-1.jpeg",
+    fullview: "img/fullview-1.jpeg",
+    alt: "alt text 1"
+  },
+  {
+    preview: "img/preview-2.jpeg",
+    fullview: "img/fullview-2.jpeg",
+    alt: "alt text 2"
+  },
+  {
+    preview: "img/preview-3.jpeg",
+    fullview: "img/fullview-3.jpeg",
+    alt: "alt text 3"
+  },
+  {
+    preview: "img/preview-4.jpeg",
+    fullview: "img/fullview-4.jpeg",
+    alt: "alt text 4"
+  },
+  {
+    preview: "img/preview-5.jpeg",
+    fullview: "img/fullview-5.jpeg",
+    alt: "alt text 5"
+  },
+  {
+    preview: "img/preview-6.jpeg",
+    fullview: "img/fullview-6.jpeg",
+    alt: "alt text 6"
+  }
+];
+const imageGallery = $qs('.js-image-gallery');
+const activeValue = 3;
+
+class Gallery {
+  constructor({ items, parentNode, defaultActiveItem }) {
+    this.items = items;
+    this.parentNode = parentNode;
+    this.defaultActiveItem = defaultActiveItem;
+    this.fullview = $cel('div', { className: "fullview" });
+    this.imgFullview = $cel("img", { src: "#", alt: "" });
+    this.list = $cel('ul', { className: "list" });
   }
 
-  const list = $qs(".list");
+  createGalleryPreview() {
+    let idx = 1;
+    for (let item of this.items) {
+      this.li = $cel('li', { className: "list__item" })
+      this.img = $cel('img', { className: `index-${idx}` })
 
-  $on(list, "click", changePreviewImg);
+      this.img.setAttribute('src', item.preview);
+      this.img.setAttribute('data-fullview', item.fullview);
+      this.img.setAttribute('alt', item.alt);
 
-  function changePreviewImg(e) {
-    const target = e.target;
-    const srcFullview = e.target.dataset.fullview;
-    console.log(target);
+      this.li.append(this.img);
+      this.list.append(this.li);
 
-    if (target.nodeName !== "IMG") return;
-    imgFullview.setAttribute("src", srcFullview);
+      idx += 1;
+    }
   }
-});
+
+  
+
+
+}
+
+
+const test = new Gallery({ items: galleryItems, parentNode: imageGallery, defaultActiveItem: activeValue })
+
+console.log(test);
+
+
