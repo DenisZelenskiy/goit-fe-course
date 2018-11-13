7d167cd3e009ed5f6814bb59e26ded7efaa1a225
Potapov Aleksey <alex.potap1811@gmail.com>
2018-11-13 20:50:50 +0200

    at 3

--- javascript/additional-task/module-11/index.js
+++ javascript/additional-task/module-11/index.js
@@ -39,33 +39,25 @@
 
 // ==========================TASK 3
 
-/* 
-  Напишите функцию validate которая проверяет поля формы 
-  firstname и lastname и возвращает результат в виде 
-  обьекта со свойствами 'first name' и 'last name'.
-  
-  Кроме того, формат объекта: в свойства записывается буль-флаг 
-  уведомляющий о статусе прохождения валидации для каждого поля.
-  {
-    'first name': true или false,
-    'last name': true или false,
-  }
-  
-  Критерии валидации:
-  1)Имя. Допускается не более 2-х слов, разделенных пробелами
-  или дефисом. Слова должны состоять только из букв.
-  
-  2)Фамилия. Допускается не более 2-х слов, разделенных пробелами
-  или дефисом. Слова должны состоять только из букв.
-  
-  При клике на кнопку submit должна происходить проверка.
-  Результат проверки, объект, выводить в консоль.
-*/
+const firstname = document.getElementById("first_name");
+const lastname = document.getElementById("last_name");
+const submitBtn = document.getElementById("submit-btn");
+const pattern = /^[a-z]+[ -][a-z]+$/;
+const result = {}
 
-// const firstname = document.getElementById("first_name");
-// const lastname = document.getElementById("last_name");
-// const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", validate);
 
-// submitBtn.addEventListener("click", validate);
+function validate(evt) {
+  evt.preventDefault();
+  const firstnameValue = firstname.value;
+  const lastnameValue = lastname.value;
 
-// function validate(evt) {}
+  const isvalidFirstname = pattern.test(firstnameValue);
+  const isvalidLastname = pattern.test(lastnameValue);
+
+  result.firstname = isvalidFirstname;
+  result.lastname = isvalidLastname;
+
+  console.log(result);
+}
+
